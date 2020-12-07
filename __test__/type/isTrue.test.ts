964b0459b755907566157f1a22b5223d94d4c909
云墨 <lg208143@alibaba-inc.com>
2020-12-07 15:17:49 +0800

    test: 💍 添加type.isTrue测试用例

---
+++ __test__/type/isTrue.test.ts
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { type } from '../../src/index';
+
+const { isTrue } = type;
+
+describe('type url isTrue', () => {
+  test('should return false if value = null', () => {
+    expect(isTrue(null)).toBe(false);
+  });
+
+  test('should return false if value = undefined', () => {
+    expect(isTrue(undefined)).toBe(false);
+  });
+
+  test('should return true if value = `true`', () => {
+    expect(isTrue('true')).toBe(true);
+  });
+});
