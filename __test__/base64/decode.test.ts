44e6fe279101e15ba64b2a856ad95e10bb7dc199
云墨 <lg208143@alibaba-inc.com>
2020-12-11 20:55:37 +0800

    fix: 🐛 添加base64相关测试用例

---
+++ __test__/base64/decode.test.ts
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+import { base64 } from '../../src/index';
+
+const { decode } = base64;
+
+describe('base64 encode', () => {
+  test('should return base64 when invoke encode params is test', () => {
+    expect(decode('dGVzdA==')).toBe('test');
+  });
+
+  test('should return base64 when invoke params is undefined', () => {
+    expect(decode()).toEqual('');
+  });
+});
---
+++ __test__/base64/encode.test.ts
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import { base64 } from '../../src/index';
+
+const { encode } = base64;
+
+describe('base64 encode', () => {
+  test('should return base64 when invoke encode params is test', () => {
+    expect(encode('test')).toBe('dGVzdA==');
+  });
+
+  test('should return base64 when invoke params is undefined', () => {
+    expect(encode()).toEqual('');
+  });
+});
+
