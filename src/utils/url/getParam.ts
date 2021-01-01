1cfe823f3bae2e1be288fac34336b7863ea0b21f
云墨 <lg208143@alibaba-inc.com>
2021-01-01 20:43:57 +0800

    feat: 🎸 添加example

--- src/utils/url/getParam.ts
+++ src/utils/url/getParam.ts
@@ -4,10 +4,16 @@
  * @remarks
  * 如果存在两个同名的参数，默认取第一个参数
  *
+ * @example
+ * ```ts
+ * import { url } from 'onex-utils';
+ * const { getUrlParams } = url;
+ * console.log(getUrlParams('key'));
+ * ```
  * @param key - The current url parameter name
  * @returns the current url parameter value with the key
  */
-export const getUrlParam = (key: string): string | null => {
+export const getUrlParam = (key: string) => {
   const reg = new RegExp(`(^|&)${key}=([^&]*)(&|$)`, 'i');
   if (!window?.location?.search) return null;
   const r = window.location.search.substr(1).match(reg);
