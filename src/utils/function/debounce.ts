72709a290452a1d0c64e0b70334a172f9af2bb00
云墨 <lg208143@alibaba-inc.com>
2021-01-15 21:58:34 +0800

    docs: ✏️ 注释更新

--- src/utils/function/debounce.ts
+++ src/utils/function/debounce.ts
@@ -3,6 +3,6 @@ import lodashDebounce from 'lodash.debounce';
 /**
  * 赋值lodash的Debounce
  *
- * @see {@link https://lodash.com/docs/4.17.15#debounce|debounce} 文档
+ * @see {@link https://lodash.com/docs/4.17.15#debounce | debounce} 文档
  */
 export const debounce = lodashDebounce;
--- src/utils/function/throttle.ts
+++ src/utils/function/throttle.ts
@@ -3,6 +3,6 @@ import lodashThrottle from 'lodash.throttle';
 /**
  * 赋值lodash的throttle
  *
- * @see {@link https://lodash.com/docs/4.17.15#throttle|throttle}
+ * @see {@link https://lodash.com/docs/4.17.15#throttle | throttle}
  */
 export const throttle = lodashThrottle;
