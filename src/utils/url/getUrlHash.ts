bf63625ecd66aee1ce40b0f430d2eccd7b5f94ec
云墨 <lg208143@alibaba-inc.com>
2021-02-03 22:10:18 +0800

    feat: 🎸 支持url上hash操作 #5

---
+++ src/utils/url/getUrlHash.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-redeclare */
+
+/**
+ * 获取当前URL上hash参数
+ *
+ * @remarks
+ * 如果URL Hash中存在两个同名的参数，默认返回第一个匹配的参数
+ * 返回结果会使用`decodeURIComponent`进行解码
+ *
+ * @example 传入`currentKey`参数
+ * ```ts
+ * import { url } from 'onex-utils';
+ * const { getUrlHash } = url;
+ * console.log(getUrlHash('bizCode'));
+ * ```
+ *
+ * @example 不传入`currentKey`参数
+ * ```ts
+ * import { url } from 'onex-utils';
+ * const { getUrlHash } = url;
+ * const hashParams = getUrlHash();
+ * console.log(hashParams);
+ * ```
+ */
+export function getUrlHash(): { [key: string]: string } | undefined;
+export function getUrlHash(currentKey: string): string | undefined;
+export function getUrlHash(currentKey?: string) {
+  const params: { [key: string]: string } = {};
+  const IHashGroup = window.location.hash.substr(1).split('&');
+
+  IHashGroup.forEach((param) => {
+    const [key, value] = param.split('=');
+
+    if (key) {
+      const existingValue = params[key];
+      if (!existingValue) {
+        params[key] = decodeURIComponent(value);
+      }
+    }
+  });
+
+  return currentKey ? params[currentKey] : params;
+}
---
+++ src/utils/url/setUrlHash.ts
@@ -0,0 +1,36 @@
+import { getUrlHash } from './getUrlHash';
+/**
+ * 设置url上对应的hash参数
+ *
+ * @remarks
+ * 当不传入`key`值，默认代表删除当前`param`
+ *
+ * @example 传入`key`、`value`值
+ * ```ts
+ * import { url } from 'onex-utils';
+ * const { setUrlHash } = url;
+ * console.log(setUrlHash('bizCode', '0234234'));
+ * ```
+ *
+ * @example 传入`key`值，不传入`value`默认删除对应key值
+ * ```ts
+ * import { url } from 'onex-utils';
+ * const { setUrlHash } = url;
+ * console.log(setUrlHash('bizCode'));
+ * ```
+ */
+export const setUrlHash = (key: string, value?: string) => {
+  const params = getUrlHash();
+  if (params?.[key] && value) {
+    params[key] = value;
+  } else if (params?.[key] && !value) {
+    delete params[key];
+  }
+  if (params) {
+    window.location.hash = Object.keys(params)
+      .map((paramKey) => {
+        return `${key}=${encodeURIComponent(params[paramKey])}`;
+      })
+      .join('&');
+  }
+};
