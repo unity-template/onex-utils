383c245a02166a03ac4b99808b128bee1a590f44
云墨 <lg208143@alibaba-inc.com>
2021-03-22 00:10:17 +0800

    feat: 🎸 添加页面路径处理

--- script/plugin/packages/alias/index.ts
+++ script/plugin/packages/alias/index.ts
@@ -1,11 +1,26 @@
 import { Application } from 'typedoc';
-import { RendererEvent } from '_typedoc@0.19.2@typedoc/dist/lib/output/events';
+import { PageEvent } from 'typedoc/dist/lib/output/events';
+
 
 export const load = (that: Application) => {
-  console.log('加载插件....');
-  that.listenTo(that.renderer, RendererEvent.BEGIN, changeName);
+  that.listenTo(that.application.renderer, {
+    [PageEvent.BEGIN]: changePageName,
+  });
 };
 
-function changeName() {
-  console.log('输出内容');
+/**
+ * 业务需要将页面的路径只保留两份，同时以文件夹的形式进行输出
+ */
+function changePageName(event: PageEvent) {
+  if (event.filename.match(/(?=_)\S+(?<=_)/g)) {
+    const name = event.filename.match(/(?=_)\S+(?<=_)/g);
+    if (name?.length) {
+      const filename = name[0].split('_').filter(i => !!i);
+      filename.splice(0, 2);
+      const newName = filename.join('/');
+      event.filename = event.filename.replace(/(?=_)\S+(?<=_)/g, newName);
+      event.url = event.url.replace(/(?=_)\S+(?<=_)/g, newName);
+      console.log(event);
+    }
+  }
 }
