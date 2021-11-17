7be86d459ad41de9f77d15275cee138409d7df9a
云墨 <lg208143@alibaba-inc.com>
2021-11-17 17:16:32 +0800

    feat: 添加prefix装饰器对应测试用例

--- __test__/validate/descriptor.test.ts
+++ __test__/validate/descriptor.test.ts
@@ -2,18 +2,32 @@ import { validate } from '../../src/index';
 
 const { AttributeDescriptor, validateInterfaceData } = validate;
 
-class ServiceDTO {
-  @AttributeDescriptor.prefix('pre-')
-  bizCode: string;
-}
 
 describe('test decorator utils', () => {
-  it('should return time', () => {
-    const data = validateInterfaceData(ServiceDTO)({
-      bizCode: '你好',
-      bizName: '阿里',
-      note: '123',
-    });
+  it('should return pre when prefix params is exist', () => {
+    class ServiceDTO {
+      @AttributeDescriptor.prefix('pre-')
+      bizCode: string;
+    }
+    const data = validateInterfaceData(ServiceDTO)({ bizCode: '你好' });
     expect(data.bizCode).toEqual('pre-你好');
+  });
+
+  it('should return pre when prefix params is not exist', () => {
+    class ServiceDTO {
+      @AttributeDescriptor.prefix()
+      bizCode: string;
+    }
+    const data = validateInterfaceData(ServiceDTO)({ bizCode: '你好' });
+    expect(data.bizCode).toEqual('你好');
+  });
+
+  it('should return pre when prefix params is symbol', () => {
+    class ServiceDTO {
+      @AttributeDescriptor.prefix(Symbol('null'))
+      bizCode: string;
+    }
+    const data = validateInterfaceData(ServiceDTO)({ bizCode: '你好' });
+    expect(data.bizCode).toEqual('Symbol(null)你好');
   });
 });
