91ccac2234c1e7c9f4bf655d81092727611f165a
云墨 <lg208143@alibaba-inc.com>
2021-11-17 17:59:49 +0800

    feat: 添加example

--- src/utils/validate/use.ts
+++ src/utils/validate/use.ts
@@ -8,8 +8,37 @@ import { RULES_KEY } from './common/key';
  *
  * @example 基础使用方式
  * ```ts
+ *   export class DataDto {
+ *     @Rule(RuleType.string().required())
+ *     @addPre('test') // 添加前缀
+ *     bizCode: string;
  *
- * ```
+ *    @Rule(RuleType.string().required())
+ *     @addPre('test') // 添加前缀
+ *     bizName: string;
+ *
+ *    @Rule(RuleType.number())
+ *     @addAlarm // 报警装饰
+ *     note?: string;
+ *   }
+ *
+ *  @ValidateComponentProps(DataDto)
+ *   class Node extends Component<DataDto> {
+ *     IRender() {
+ *       console.log(this.props.bizCode, this.props.bizName, this.props.note);
+ *     }
+ *   }
+ *
+ *  new Node({
+ *     bizCode: '你好',
+ *     bizName: '世界',
+ *   }).IRender();
+ *
+ *    * output:
+ *    * Debugger attached.
+ *    * DataDto.note: 参数告警
+ *    * test-你好 test-世界 undefined
+ *```
  */
 export function ValidateAndTransformComponentProps(ClassFto) {
   return (IFunction) =>
@@ -36,7 +65,28 @@ export function ValidateAndTransformComponentProps(ClassFto) {
  * 校验服务端数据的使用方式
  * @example 基础使用方式
  * ```ts
+ *   export class DataDto {
+ *     @Rule(RuleType.string().required())
+ *     @addPre('test') // 添加前缀
+ *     bizCode: string;
  *
+ *    @Rule(RuleType.string().required())
+ *     @addPre('test') // 添加前缀
+ *     bizName: string;
+ *
+ *    @Rule(RuleType.number())
+ *     @addAlarm // 报警装饰
+ *     note?: string;
+ *   }
+ *
+ *  const result = validateInterfaceData(DataDto)({
+ *     bizCode: '你好',
+ *     bizName: '世界',
+ *     note: '213',
+ *   });
+ *
+ *  console.log(result.bizCode, result.bizCode, result.note);
+ *   // console.log test-你好 test-你好 213
  * ```
  */
 export function validateInterfaceData(ClassFto) {
@@ -58,7 +108,37 @@ export function validateInterfaceData(ClassFto) {
  * 通过HOC的方式检验组件服务端数据
  * @example 基础使用方式
  * ```ts
+ *    export class DataDto {
+ *      @Rule(RuleType.string().required())
+ *      @addPre('test') // 添加前缀
+ *      bizCode: string;
  *
+ *      @Rule(RuleType.string().required())
+ *      @addPre('test') // 添加前缀
+ *      bizName: string;
+ *
+ *      @Rule(RuleType.number())
+ *      @addAlarm // 报警装饰
+ *      note?: string;
+ *    }
+ *
+ *    class Node extends Component<DataDto> {
+ *      IRender() {
+ *        console.log(this.props.bizCode, this.props.bizName, this.props.note);
+ *      }
+ *    }
+ *
+ *    const INode = ValidateComponentPropsHoc(DataDto)(Node)
+ *
+ *    new INode({
+ *      bizCode: '你好',
+ *      bizName: '世界',
+ *    }).IRender();
+ *
+ *     * output:
+ *     * Debugger attached.
+ *     * DataDto.note: 参数告警
+ *     * test-你好 test-世界 undefined
  * ```
  */
 export function validateComponentPropsHoc(dto) {
